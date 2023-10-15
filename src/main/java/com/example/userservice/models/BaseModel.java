4626b0281d5815e472155f976ab42901f21db459
Stryker316 <ansil.mishra316@gmail.com>
2023-10-15 21:45:53 +0530

    connect to user_service DB in MySql and build the tables

--- src/main/java/com/example/userservice/models/BaseModel.java
+++ src/main/java/com/example/userservice/models/BaseModel.java
@@ -3,7 +3,9 @@ package com.example.userservice.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
